c95d36b949c211dc8a54be76aa9924ce4eba4a7d
shwet tiwari <shwetst685@gmail.com>
2022-06-10 01:26:41 +0530

    Add files via upload

---
+++ wordle-index.js
@@ -0,0 +1,51 @@
+const PORT =8000
+
+const axios = require("axios").default
+const express = require("express")
+const cors= require("cors")
+require('dotenv').config()
+const app= express()
+app.use(cors())
+app.get('/word',(re,res)=>{
+    const options = {
+        method: 'GET',
+        url: 'https://random-words5.p.rapidapi.com/getMultipleRandom',
+        params: {count: '5', wordLength: '5'},
+        headers: {
+          'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+          'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
+        }
+      }
+      
+      axios.request(options).then( (response)=> {
+          console.log(response.data)
+          res.json(response.data[0])
+      }).catch((error)=> {
+          console.error(error)
+      })
+
+})
+
+app.get('/check',(req,res)=>{
+    const word = req.query.word
+
+    const options = {
+        method: 'GET',
+        url: 'https://twinword-word-graph-dictionary.p.rapidapi.com/association/',
+        params: {entry: word},
+        headers: {
+          'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+          'X-RapidAPI-Host': 'twinword-word-graph-dictionary.p.rapidapi.com'
+        }
+      }
+      
+      axios.request(options).then((response)=> {
+          console.log(response.data)
+          res.json(response.data.result_msg)
+      }).catch( (error)=> {
+          console.error(error)
+      })
+})
+
+app.listen(PORT,()=> console.log('server running on port'+PORT))
+
