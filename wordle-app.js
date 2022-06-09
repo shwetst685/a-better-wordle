7e2f5715cd85b0f5db16fdd932929426ebc1d77d
shwet tiwari <shwetst685@gmail.com>
2022-06-09 18:27:44 +0530

    Add files via upload

---
+++ wordle-app.js
@@ -0,0 +1,149 @@
+const tileDisplay= document.querySelector('.tile-container')
+const keyboard= document.querySelector('.key-container')
+const messageDisplay= document.querySelector('.message-container')
+
+const wordle= "SUPER"
+const keys =[
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'ENTER',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+    '<<',
+]
+const guessRows=[
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','','']
+]
+
+let currentRow=0
+let currentTile =0
+let isGameOver =false
+
+guessRows.forEach((guessRow, guessRowIndex) =>{
+    const rowElement = document.createElement('div')
+    rowElement.setAttribute('id','guessRow-'+guessRowIndex)
+    guessRow.forEach((guess,guessIndex)=>{
+        const tileElement= document.createElement('div')
+        tileElement.setAttribute('id','guessRow-'+guessRowIndex+'-tile-'+guessIndex)
+        tileElement.classList.add('tile')
+        rowElement.append(tileElement)
+    })
+    tileDisplay.append(rowElement)
+})
+
+
+keys.forEach(key =>{
+    const buttonElement= document.createElement('button')
+    buttonElement.textContent=key
+    buttonElement.setAttribute('id',key)
+    buttonElement.addEventListener('click',() => handleClick(key))
+    keyboard.append(buttonElement)
+})
+
+const handleClick = (key) =>{
+    console.log('clicked',key)
+    if(key=== "<<"){
+        deleteLetter()
+        return
+    }
+    if (key === "ENTER"){
+        chechRow()
+        return
+    }
+    addLetter(key)
+}
+
+const addLetter = (key)=>{
+    if (currentTile<5 && currentRow<6){
+    const tile = document.getElementById('guessRow-'+currentRow+'-tile-'+currentTile)
+    tile.textContent=key
+    guessRows[currentRow][currentTile]=key
+    tile.setAttribute('data',key)
+    currentTile++
+    console.log(guessRows[currentRow])
+    }
+}
+
+const deleteLetter = () =>{
+    if(currentTile>0){
+    currentTile--
+    const tile = document.getElementById('guessRow-'+currentRow+'-tile-'+currentTile)
+    tile.textContent =''
+    guessRows[currentRow][currentTile]=''
+    tile.setAttribute('data','')
+    }
+}
+
+const chechRow= ()=>{
+    const guess= guessRows[currentRow].join('')
+    if (currentTile>4){
+        
+        console.log('guess is '+ guess, 'wordle is '+ wordle)
+        flipTile()
+        if(guess==wordle){
+            showMessage('Magnificient')
+            isGameOver=true
+            return
+        }else{
+            if(currentRow>=5){
+                isGameOver=true
+                showMessage("Game Over")
+            }
+            if(currentRow<5){
+                currentRow++
+                currentTile=0
+            }
+        }
+    }
+}
+const showMessage=(message)=>{
+    const messageElement = document.createElement('p')
+    messageElement.textContent= message
+    messageDisplay.append(messageElement)
+    setTimeout(()=> messageDisplay.removeChild(messageElement),2000)
+}
+
+const flipTile=()=>{
+    const rowTiles= document.querySelector('#guessRow-'+currentRow).childNodes
+    rowTiles.forEach((tile,index)=>{
+        const dataLetter= tile.getAttribute('data')
+        setTimeout(()=> {
+        tile.classList.add('flip')
+        if (dataLetter==wordle[index]){
+            tile.classList.add('green-overlay')
+        }
+        else if(wordle.includes(dataLetter)){
+            tile.classList.add('yellow-overlay')
+        }
+        else {
+            tile.classList.add('grey-overlay')
+        }},500)
+    })
+}
